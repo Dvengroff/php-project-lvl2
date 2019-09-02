cd6f0afb41460fa22389666aa686b756a2c1ee80
Dmitry Grebenschikov <dvengroff@gmail.com>
2019-09-02 23:43:57 +0400

    fix version

--- src/Runner.php
+++ src/Runner.php
@@ -6,7 +6,7 @@ use function GenDiff\Differ\genDiff;
 
 function run($doc)
 {
-    $args = \Docopt::handle($doc, ['version' => 'GenDiff. Version 0.5.0']);
+    $args = \Docopt::handle($doc, ['version' => 'GenDiff. Version 0.6.0']);
     
     try {
         $diff = genDiff($args['<firstFile>'], $args['<secondFile>'], $args['--format']);
